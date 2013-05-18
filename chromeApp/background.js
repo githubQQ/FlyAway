c8794034d3667fa9b64cd6dd2204a945efdfba8c
ngjaying <ngjaying@gmail.com>
2013-05-18 19:33:35 +0800

    Reorganize the project structure

--- chromeApp/background.js
+++ chromeApp/background.js
@@ -3,7 +3,7 @@
  * Date: 13-5-12
  * Time: 下午6:43
  */
-chrome.app.runtime.onLaunched.addListener(function() {
+chrome.app.runtime.onLaunched.addListener(function () {
     chrome.app.window.create('index.html', {
         'width': screen.width,
         'height': screen.height
